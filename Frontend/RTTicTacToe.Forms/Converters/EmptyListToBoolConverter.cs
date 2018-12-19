bd976278ec4f225cd6862e252005c273f3d33bba
José Pereira <leaopereira@gmail.com>
2018-12-19 16:02:00 +0100

    Small fixes for UI with XF4

---
+++ Frontend/RTTicTacToe.Forms/Converters/EmptyListToBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RTTicTacToe.Forms.Converters
+{
+    public class EmptyListToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = value as IList;
+
+            if (enumerable == null)
+            {
+                return true;
+            }
+
+            return enumerable.Count == 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
